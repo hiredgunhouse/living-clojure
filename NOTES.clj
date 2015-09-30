6470948237ba72e5954aa648d27ab3690685ff4c
Piotr Owsiak <piotr.owsiak@o2.pl>
2015-10-01 00:18:04 +0200

    conditionals

--- NOTES.clj
+++ NOTES.clj
@@ -427,3 +427,118 @@ af/fav-food
 
 (some #{false} [false false false])
 ;; -> nil
+
+(if true "it is true" "it is false")
+;; -> "it is true"
+
+(if false "it is true" "it is false")
+;; -> "it is false"
+
+(if nil "it is true" "it is false")
+;; -> "it is false"
+
+(if (= :drinkme :drinkme)
+  "Try it"
+  "Dont try it")
+;; -> "Try it"
+
+(let [need-to-grow-small (> 5 3)]
+  (if need-to-grow-small
+    "drink bottle"
+    "don't drink bottle"))
+;; -> "drink bottle"
+
+(if-let [need-to-grow-small (> 5 1)]
+  "drink bottle"
+  "don't drink bottle")
+;; -> "drink bottle"
+
+(if-let [need-to-grow-small (> 5 1)]
+  ["drink bottle" need-to-grow-small]
+  "don't drink bottle")
+;; -> ["drink bottle" true]
+
+(defn drink [need-to-grow-small]
+  (when need-to-grow-small "drink bottle"))
+;; -> #'user/drink
+
+(drink true)
+;; -> "drink bottle"
+
+(drink false)
+;; -> nil
+
+(when-let [need-to-grow-small true]
+  "drink bottle")
+;; -> "drink bottle"
+
+(when-let [need-to-grow-small false]
+  "drink bottle")
+;; -> nil
+
+(let [bottle "drinkme"]
+  (cond
+    (= bottle "poison") "don't touch"
+    (= bottle "drinkme") "grow smaller"
+    (= bottle "empty") "all gone"))
+
+;; -> "grow smaller"
+
+(let [x 5]
+  (cond
+    (> x 10) "bigger than 10"
+    (> x 4) "bigger than 4"
+    (> x 3) "bigger than 3"))
+;; -> "bigger than 4"
+
+(let [x 5]
+  (cond
+    (> x 3) "bigger than 3"
+    (> x 10) "bigger than 10"
+    (> x 4) "bigger than 4"))
+;; -> "bigger than 3"
+
+(let [x 1]
+  (cond
+    (> x 3) "bigger than 3"
+    (> x 10) "bigger than 10"
+    (> x 4) "bigger than 4"))
+;; -> nil
+
+(let [bottle "mystery"]
+  (cond
+    (= bottle "poison") "don't touch"
+    (= bottle "drinkme") "grow smaller"
+    (= bottle "empty") "all gone"
+    :else "unknown"))
+;; -> "unknown"
+
+(let [bottle "mystery"]
+  (cond
+    (= bottle "poison") "don't touch"
+    (= bottle "drinkme") "grow smaller"
+    (= bottle "empty") "all gone"
+    "default" "unknown"))
+;; -> "unknown"
+
+(let [bottle "drinkme"]
+  (case bottle
+    "poison" "don't touch"
+    "drinkme" "grow smaller"
+    "empty" "all gone"))
+;; -> "grow smaller"
+
+(let [bottle "mystery"]
+  (case bottle
+    "poison" "don't touch"
+    "drinkme" "grow smaller"
+    "empty" "all gone"))
+;; -> IllegalArgumentException No matching clause: mystery
+
+(let [bottle "mystery"]
+  (case bottle
+    "poison" "don't touch"
+    "drinkme" "grow smaller"
+    "empty" "all gone"
+    "unknown"))
+;; -> "unknown"
